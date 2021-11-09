31cfa17467ae020a1ecb6884f82ddc9f27a1d6f8
Khailas R <khailas303@gmail.com>
2021-11-09 23:26:44 +0530

    updated

--- In Depth/O_of_n_square O(n^2).js
+++ In Depth/O_of_n_square O(n^2).js
@@ -1,11 +1,15 @@
 // O(n^2)
+// 2 dimensional array with rows and columns
 const square = ((n) => {
     for (let i = 0; i < n; i++) {
         // nested for loop
         for (let j = 0; j < n; j++) { 
-            console.log(i, j);
+            // i = column and j = row
+            console.log(i, j);  
         }
     }
 });
 
 square(5);
+
+// https://www.youtube.com/watch?v=Mo4vesaut8g 7:30 onwards
