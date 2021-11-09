cf752342514c0c0cd131c98cb94c3e9b676fe6ec
Khailas R <khailas303@gmail.com>
2021-11-09 14:53:30 +0530

    initial commit

---
+++ Initial/app.js
@@ -0,0 +1,43 @@
+// O(1) -> constant
+const x = 4 + 3;
+
+// O(n) -> linear
+for (let i = 0; i < n; i++) {
+
+}
+
+// O(n) -> linear
+let x = 0;
+for (let i = 0; i < n; i++) {
+    x += 1;
+}
+
+// O(n) -> linear
+for (let i = 0; i < n; i++) {}
+for (let i = 0; i < n; i++) {}
+for (let i = 0; i < n; i++) {}
+for (let i = 0; i < n; i++) {}
+for (let i = 0; i < n; i++) {}
+for (let i = 0; i < n; i++) {}
+
+
+// O(n²) -> quadratic
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < y; j++) {
+
+    }
+}
+
+// O(log n)
+while (n > 0) {
+    n /= 2;      // divided by 2
+}
+
+// O(n log n)
+// O(n)
+for (let i = 0; i < n; i++) {
+    // O(log n)
+    while (x > 0) {
+        x /= 2;
+    }
+}
