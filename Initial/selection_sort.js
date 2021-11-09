4b4ae44f7df9709ce560c8e6cb07cba762263df0
Khailas R <khailas303@gmail.com>
2021-11-09 18:42:50 +0530

    initial commit

---
+++ Initial/selection_sort.js
@@ -0,0 +1,22 @@
+const swap = ((array, i, j) => {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+});
+
+const selectionSort = ((array) => {
+    for (let i = 0; i < array.length; i++) {
+        let min = i;
+
+        // O(n) -> linrar
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+        if (i != min) {
+            swap(array, i, min);
+        }
+    }
+    return array;
+});
