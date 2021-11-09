1b2a5ba0b5d1c5f3e0582f8d67bb6db70bbe8773
Khailas R <khailas303@gmail.com>
2021-11-09 18:43:11 +0530

    update

--- Initial/selection_sort.js
+++ Initial/selection_sort.js
@@ -8,7 +8,7 @@ const selectionSort = ((array) => {
     for (let i = 0; i < array.length; i++) {
         let min = i;
 
-        // O(n) -> linrar
+        // O(n) -> linear
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[min]) {
                 min = j;
