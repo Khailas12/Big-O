82fd812049d2964081c7d656ca95d3ecae3c9af7
Khailas R <khailas303@gmail.com>
2021-11-11 13:54:54 +0530

    updated and defined

--- In_depth/6. O(n log n).js
+++ In_depth/6. O(n log n).js
@@ -1,11 +1,18 @@
 const nLogN = ((n) => {
-    let y = n;
-    while (n > 1) {
-        n = Math.floor(n / 2);
-        for (let i = 1; i <= y; i++) {
+    let y = n;  
+    while (n > 1) {     // n = 4
+        n = Math.floor(n / 2);  // 4 / 2 = 2
+        
+        // 'y'still remains 4 comparing with the n which got divided prior into 2.
+        for (let i = 0; i <= y; i++) {
             console.log(i);
         }
     }
 });
 
-nLogN(n)
+nLogN(4);
+
+// O(n log n) = O(n * log n)
+
+// both while and for loop has the complexity of O(n) considering that both gets iterated by the full size of y. which is 4 as assigned. so the splitted n = 4 into 2 will also get iterated 4 times.
+// processing time increases linearly with the size of n.
