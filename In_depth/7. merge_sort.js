934a4f0ca87b8819cafefa1d35c6b6411e29dd64
Khailas R <khailas303@gmail.com>
2021-11-12 20:41:05 +0530

    updated

--- In_depth/7. merge_sort.js
+++ In_depth/7. merge_sort.js
@@ -2,20 +2,21 @@ const mergeSort = ((array) => {
     if (array.length < 2) {
         return array;
     }
-
+    
     const middleIndex = Math.floor((array.length) / 2);
     const leftArray = array.slice(0, middleIndex);
-    const rightArray = array.slide(middleIndex, array.length);
+    const rightArray = array.slice(middleIndex, array.length);
     
     // returning the result of the call to the function
     return merge(mergeSort(leftArray), mergeSort(rightArray)); // recursive call
 });
 
+
 const merge = ((leftArray, rightArray) => {
     let resultArray = [];
     let leftIndex = 0;
     let rightIndex = 0;
-
+    
     while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
         if (leftArray[leftArray] < rightArray[rightIndex]) {
             resultArray.push([leftIndex])
@@ -26,7 +27,11 @@ const merge = ((leftArray, rightArray) => {
             rightIndex += 1;
         }
     }
-})
+    return resultArray.concat(leftArray.slice(leftIndex)).concat(rightArray.slice(rightIndex));
+});
+
+let array = [12, 4, 5, 7, 9, 1];
+console.log(mergeSort(array));
 
 // in slice it exclude the last value in the array 
 // [1, 2, 3] => so it slices the array excluding the last value in the array, despite the start or index value remains untouched.
